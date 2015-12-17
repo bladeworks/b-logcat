c926388f88a60a4926b3dbd44d694dc6168987dd
Blade <flyblade@gmail.com>
2015-12-16 16:25:45 -0800

    Add some description.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Overview
+
+B-logcat is a small tool to view / filter the android logs. 
+<add a gif here>
+
+# Install
+
+
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
         name='blade-logcat',
         version='1.0',
+        author='Blade Liu',
+        author_email='flyblade@gmail.com',
+        url='https://github.com/bladeworks/b-logcat'
         py_modules=['logcat'],
         include_package_data=True,
         install_requires=[
